b7e644d5a5dd00a78b6a831d7dbb92ab903df53b
aviran355 <aviran.ma@gmail.com>
2022-12-25 15:28:55 +0200

    update azure 6

--- commit_2.sh
+++ commit_2.sh
@@ -1,3 +1,6 @@
-git log -1 --decorate | awk '/tag:/ {print $2}' | xargs read tags
-echo $tags
-
+tags=($(git tag --points-at HEAD))
+for tag in $tags; do
+  eval "tag_$tag=\$tag"
+done
+echo "Tag 1: ${tags[0]}"
+echo "Tag 2: ${tags[1]}"
