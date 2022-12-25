74184eecc153e8bbc04a30a82212d33d0c5a25ab
aviran355 <aviran.ma@gmail.com>
2022-12-25 15:45:36 +0200

    azuretest8

--- commit_2.sh
+++ commit_2.sh
@@ -1,6 +1,17 @@
-tags=($(git tag --points-at HEAD))
+# Get the list of tags
+tags=$(git tag --points-at HEAD)
+
+# Set the tags in variables
 for tag in $tags; do
   eval "tag_$tag=\$tag"
 done
-echo "Tag 1: ${tags[0]}"
-echo "Tag 2: ${tags[1]}"
+
+#echo "$tag"
+
+# You can then access the tags like this
+echo "Tag 1: $tag_1"
+echo "Tag 2: $tag_2"
+echo "Tag 3: $tag_3"
+echo "Tag 4: $tag_4"
+# and so on
+
