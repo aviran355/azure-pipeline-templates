535fffb5cd06c7a61315d9d2fc6d9b8880a096a0
aviran355 <aviran.ma@gmail.com>
2022-12-28 11:58:19 +0200

    azuretest90

--- commit_2.sh
+++ commit_2.sh
@@ -1,17 +1,5 @@
-# Get the list of tags
 tags=$(git tag --points-at HEAD)
-
-# Set the tags in variables
-for tag in $tags; do
-  eval "tag_$tag=\$tag"
-done
-
-#echo "$tag"
-
-# You can then access the tags like this
-echo "Tag 1: $tag_1"
-echo "Tag 2: $tag_2"
-echo "Tag 3: $tag_3"
-echo "Tag 4: $tag_4"
-# and so on
-
+#for tag in $tags; do
+#  eval "tag_$tag=\$tag"
+#done
+echo "$tags"
