20fa44e895ed467dfa3fe87ee9e536e19ad7fb08
aviran355 <aviran.ma@gmail.com>
2023-01-11 14:16:52 +0200

    azuretest99

--- commit_2.sh
+++ commit_2.sh
@@ -1,5 +1,15 @@
-tags=$(git tag --points-at HEAD)
+#tags=$(git tag --points-at HEAD)
 #for tag in $tags; do
 #  eval "tag_$tag=\$tag"
 #done
-echo "$tags"
+#echo "$tags"
+
+# Get the SHA of the last commit
+LAST_COMMIT_SHA=$(git rev-parse HEAD)
+
+# Get a list of all tags that point to the last commit
+TAGS=$(git tag --points-at $LAST_COMMIT_SHA)
+
+# Print the list of tags
+echo "$TAGS"
+
